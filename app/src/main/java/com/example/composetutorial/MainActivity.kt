37890132049580f2d2e66c82707f29a8694feb5d
Steven Flintham <sgf@lemma.co.uk>
2025-06-05 18:35:59 +0100

    Imperfect lose focus when clicking off combo text box

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.composetutorial
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -50,25 +51,28 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.core.view.WindowCompat
 
 enum class ThemePreference {
-    LIGHT,
-    DARK,
-    SYSTEM
+    LIGHT, DARK, SYSTEM
 }
 
 // TODO: This probably needs to track state/events via parents
 // TODO: A final version of this might want an internal (database, not linear) ID for each item and it might expose that ID as well as/instead of the associated String to the caller, but the ID is of course invisible to the UI
 @Composable
-fun ComboBox(label: String,
-             value: String,
-             onValueChange: (String) -> Unit,
-             content: List<String>, // TODO: poor name for var?
-             modifier: Modifier = Modifier) {
+fun ComboBox(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    content: List<String>, // TODO: poor name for var?
+    modifier: Modifier = Modifier
+) {
     // TODO: I suspect we may not want to pass all value changes in the text box through to the onValueChange provided by our parent. We are encouraging the user to type partial substrings which are meaningless to the parent, and it also doesn't really care anyway until we have "finished" and have a possibly-valid (but we may not) string, either because the user typed it or because they clicked it in the list. For now I am not even trying to call the parent and just ignoring the value they supply.
     var isExpanded by remember { mutableStateOf(false) }
-    var text by rememberSaveable { mutableStateOf( "" ) }
+    var text by rememberSaveable { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
     Column() {
         Row(modifier = modifier) {
             TextField(
@@ -81,36 +85,63 @@ fun ComboBox(label: String,
                         contentDescription = null,
                         modifier = Modifier.clickable {
                             isExpanded = !isExpanded
-                        }
-                    )
-                }
-            )
+                        })
+                })
         }
         // TODO: I may need to use LazyColumn inside DropdownMenu to get laziness, given my list could have approx 100 items. Not worrying about that just now.
         DropdownMenu(
-            expanded= isExpanded,
-            onDismissRequest = { isExpanded= false},
-            ) {
-                    DropdownMenuItem(text = { Text("bazzle") }, onClick = {})
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false },
+        ) {
+            for (item in content) {
+                DropdownMenuItem(text = { Text(item) }, onClick = {
+                    text = item
+                    isExpanded = false
+                    focusManager.clearFocus()
+                })
             }
+        }
     }
 }
 
-@Preview(showBackground=true)
+@Preview(showBackground = true)
 @Composable
 fun PreviewComboBox() {
-    ComboBox(label = "Label3", value="Value3", onValueChange = {}, content=listOf("foo", "bar", "baz"))
+    ComboBox(
+        label = "Label3",
+        value = "Value3",
+        onValueChange = {},
+        content = listOf("foo", "bar", "baz")
+    )
 }
 
 @Preview
 @Composable
 fun TripleComboBox() {
     Row() {
-        ComboBox(label = "Category", value = "Foo", onValueChange = {}, content=listOf(), modifier = Modifier.weight(1f))
+        ComboBox(
+            label = "Category",
+            value = "Foo",
+            onValueChange = {},
+            content = listOf(),
+            modifier = Modifier.weight(1f)
+        )
         Spacer(modifier = Modifier.width(4.dp))
-        ComboBox(label = "Item", value = "Bar", onValueChange = {}, content=listOf(), modifier = Modifier.weight(1f))
+        ComboBox(
+            label = "Item",
+            value = "Bar",
+            onValueChange = {},
+            content = listOf(),
+            modifier = Modifier.weight(1f)
+        )
         Spacer(modifier = Modifier.width(4.dp))
-        ComboBox(label = "Source", value = "Baz", onValueChange = {}, content=listOf(), modifier = Modifier.weight(1f))
+        ComboBox(
+            label = "Source",
+            value = "Baz",
+            onValueChange = {},
+            content = listOf(),
+            modifier = Modifier.weight(1f)
+        )
     }
 }
 
@@ -209,12 +240,11 @@ fun MessageCard(msg: Message) {
     }
 }
 
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Log.d("TODO2", "TODO2")
         // TODO: Experiment with adding a Settings activity and make the dark/light/follow system available and grey out (with some text saying why) follow system on Android < 10
         val isDarkTheme = true /* TODO when (userThemePref) {
             ThemePreference.DARK -> true
@@ -222,13 +252,23 @@ class MainActivity : ComponentActivity() {
             ThemePreference.SYSTEM -> isSystemInDarkTheme()
         } */
         setContent {
+            val focusManager = LocalFocusManager.current
             Box(Modifier.safeDrawingPadding())
-            ComposeTutorialTheme(
-                /* darkTheme = isDarkTheme */) {
+            ComposeTutorialTheme(/* darkTheme = isDarkTheme */) {
                 Scaffold(modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)) {
                     Column(modifier = Modifier.padding(it)) {
                         ComboBox("Label", "Value", onValueChange = {}, content = listOf("c1", "c2"))
-                        Conversation(SampleData.conversationSample)
+                        Box(modifier = Modifier.pointerInput(Unit) {
+                            awaitPointerEventScope {
+                                while (true) {
+                                    val event = awaitPointerEvent()
+                                    Log.d("POINTER", "Got pointer: $event")
+                                    focusManager.clearFocus()
+                                }
+                            }
+                        }) {
+                            Conversation(SampleData.conversationSample)
+                        }
                     }
                 }
             }
@@ -238,9 +278,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview(name = "Light Mode")
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
 )
 @Composable
 fun PreviewMessageCard() {
