3203efd47a1479dc193184994ffa038225b60969
Steven Flintham <sgf@lemma.co.uk>
2025-06-11 21:43:06 +0100

    Reinstate vertical scroll on home screen

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -890,6 +890,7 @@ fun TodoRenameMe(navController: NavHostController) {
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
+                    .verticalScroll(androidx.compose.foundation.rememberScrollState())
             ) {
                 MainScreen() // TODO: rename this
 
@@ -1180,5 +1181,3 @@ fun AppNavigation() {
 // TODO: There is no colour in the app at all when running on P7! Material You active without me realising it?
 
 // TODO: There is an ugly animation glitch where the "T" of "TODO" on SettingsScreen hangs around far too long when transitioning between home and settings. This wasn't visible before I added the previously-lost border at the left and right of the main activity. It feels like this might be a clue to some problem with the animations but I am far from sure. In practice this will probably not be an issue if we add the same border to settings, but I am not sure that's a "fix" even if it does happen.
-
-// TODO: I have lost the screen vertical scrolling in all my reworking...
