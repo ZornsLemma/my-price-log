5f22cf8b7a61015a8b7f1175e0a876637ae76def
Steven Flintham <sgf@lemma.co.uk>
2025-07-02 12:20:09 +0100

    WIP NumericTextField

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -2909,19 +2909,36 @@ fun CurrencyTextField(
     )
 }
 
+// TODO: Maybe rename - the idea here is this does not insist the input is actually parseable as a
+// decimal (for example, we allow "24.2.3" so the user can enter a new decimal point *and then later
+// go delete the old one*), but that it rejects obviously incorrect things. We allow digits, commas,
+// full stops and spaces - the interpretation of these is locale-dependent, but this should allow
+// valid decimals to be entered with no annoying quirks in any locale.
+fun isValidTransitionalDecimal(input: String): Boolean {
+    // Regular expression to match any character that is not a digit, comma, period, or space
+    val regex = Regex("[^\\d,.\\s]")
+    return !regex.containsMatchIn(input)
+}
+
 @Composable
 fun NumericTextField(
     label: @Composable() (() -> Unit)? = null,
     value: TextFieldValue,
+    onCandidateValueChange: ((String) -> Boolean) = { isValidTransitionalDecimal(it) },
     onValueChange: (TextFieldValue) -> Unit,
     modifier: Modifier = Modifier
 ) {
     TextField(
         label = label,
         value = value,
-        onValueChange = onValueChange, // TODO!
+        onValueChange = { newValue ->
+            if (onCandidateValueChange(newValue.text)) {
+                // TODO: MORE VALIDATION-WITHOUT-REJECTION HERE
+                onValueChange(newValue)
+            }
+        },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
