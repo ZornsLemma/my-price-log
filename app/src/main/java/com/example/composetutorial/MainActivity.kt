8d44f2f110c8cb2370fa32ce1932e54879f62fe3
Steven Flintham <sgf@lemma.co.uk>
2025-07-04 00:43:36 +0100

    Add TODO

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -2937,6 +2937,8 @@ val foo: TextFieldValue = TextFieldValue("string")
 // TODO: Do we want support for "less scary" non-red supportingText with different icon? Probably OK without but think about it or add later if necessary.
 // TODO: ChatGPT semi-magic
 
+// TODO: I think I need to try moving the validation logic into the viewmodel as per one of my chatgpt conversations.
+
 // TODO: It occurs to me that this isn't really "numeric" - it is a *Validated*TextField. Which is in many ways better of course.
 // I could imagine having different sets of validations the caller passes in to change what it's used for. Although composables
 // aren't really "objects", I do wonder if there would be some way to have a parallel "validate and convert to target type"
