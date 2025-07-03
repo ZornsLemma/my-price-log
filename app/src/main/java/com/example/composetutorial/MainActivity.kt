963ed2e72c7aa3e4687b8c5efd1613af6cca08d9
Steven Flintham <sgf@lemma.co.uk>
2025-07-03 01:20:47 +0100

    Add TODO

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -2576,6 +2576,7 @@ fun HomeScreenScaffold(
 @Composable
 // TODO: https://m3.material.io/components/dialogs/specs says (near bottom) top/left/right padding on a full screen dialog should be 24.dp - I am probably not doing that, should I? Should I use similar padding on "non-dialog full screens" to match??
 // TODO: This should probably show the confirmed date "in full", and although we will probably use internal logic to update it rather than letting user choose, we could show it live changing to "now" when the non-notes fields are different than originalprice, and back to old value if they rever them, and maybe a "x-in-circle" or some other button to say "force this back to the old date", though less sure that is a good idea - but showing the confirm date we are going to use is maybe a good way to show feedback that it will change. we could maybe use relative date just as on main screen, or show both?!
+// TODO: I was thinking this screen would show the price history, but I am cooling on that. Not quite sure where we would show it, but I am not sure it's something we want cluttering up this in-store edit screen, or encouraging people to go into this "live edit" view where they might accidentally change data just to see the history. Maybe this could go on the overflow menu on home screen if we have all three things selected?
 fun OuterFullScreenDialog(
     vm: PriceTrackerViewModel, // TODO: Maybe this should have its own ViewModel?
     navController: NavHostController,
