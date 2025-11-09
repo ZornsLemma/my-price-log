647cd4e5ea14c08cc99ecafbb93274a1e6b8385b
Steven Flintham <sgf@lemma.co.uk>
2025-11-09 12:10:25 +0000

    Lock to portrait mode on phones

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.compose.foundation.layout.padding
 import android.app.Application
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.database.sqlite.SQLiteDatabase
 import android.icu.text.Collator
@@ -7244,11 +7245,33 @@ fun LicenseText(licenseText: String) {
     )
 }
 
+// TODO: ChatGPT magic
+private fun Context.isPhone(): Boolean {
+    val metrics = resources.configuration
+    val smallestWidthDp = metrics.smallestScreenWidthDp
+    return smallestWidthDp < 600  // Common cutoff: <600dp = phone
+}
+
 // TODO: This is a bit of a mess but probably best leave it alone until I either gain more
 // experience or do more testing with different Android versions.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // I understand Google are discouraging apps from simply locking to portrait orientation, to
+        // support tablets/ChromeOS devices better. This app is probably always going to want to run
+        // in portrait on a (non-foldable) phone and I don't see any value in putting effort into
+        // layouts to allow landscape to work properly on a phone. ChatGPT suggested locking to
+        // portrait only on phones and I think that's a reasonable compromise for now. I haven't
+        // tested on tablets or similar devices, but I suspect the app will work fine on larger
+        // screens in portrait or landscape, even if maybe looks a bit odd.
+        // ENHANCE: In the future it might be nice to add alternative layouts to work better on
+        // larger devices like tablets or foldables in both landscape and portrait mode. This is
+        // probably not a common use case though.
+        if (isPhone()) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+
         // Target SDK >=35 directly enables edge-to-edge (see e.g. https://stackoverflow.com/questions/79018063/trying-to-understand-edge-to-edge-in-android). We don't particularly want this, but we can work with it so we don't try to fight it.
         // We call it here to be explicit. TODO: I am far from clear but you can pass some arguments to enableEdgeToEdge(), which may have some relevant effect on older and/or newer platforms. For now I will keep it simple but if there are nightmarish inconsistencies on older versions of Android this might be part of the puzzle.
         enableEdgeToEdge()
