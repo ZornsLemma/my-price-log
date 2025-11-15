92600666a9d29ec61fbf198b70b8c816f0f5fde0
Steven Flintham <sgf@lemma.co.uk>
2025-11-15 15:39:14 +0000

    Move some comments into coding-notes.md

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -8386,55 +8386,6 @@ fun ViewPriceHistoryScreen(
     }
 }
 
-// General note type comments to put somewhere appropriate in long term:
-
-
-// Full-screen dialog implementation notes:
-//
-// I must have written this out in comments or git commit messages or questions to LLMs multiple
-// times but for the record (and writing a few days after I finally "solved" it, so my memory might
-// be imperfect) as of right now the best way to implement this seems to be to fake it, having the
-// full-screen dialog actually be a full screen composable accessed through the regular app
-// navigation structure. The level of actual trickery to make this work is relatively small - really
-// just that the enter/exit transition needs to be a dialog-like vertical slide, not a sibling-like
-// horizontal slide. The full-screen dialog ought to have a dialog-style top bar with a close button
-// and a "confirm" button and the back button/gesture needs overriding to behave like the close
-// button, but those would probably be necessary however it's implemented.
-//
-// The other suggestions I received and tried very very hard to implement were:
-//
-// Dialog: This is the obvious way to do it. The documentation does note that it's not intended for
-// full-screen dialogs. The killer problem for me here was that since I needed keyboard input in my
-// dialog, I had to allow for the on-screen keyboard sliding in and this really seemed to interact
-// badly, even though it was near trivial to get it to work in normal full-screen composables.
-//
-// Popup: This does (I think) "guarantee" that the stuff on the popup is "on top", although it still
-// requires finicky hacks to trap focus and avoid touch input sometimes going to the screen
-// underneath. The killer problem for me was that a simple editable TextField didn't work on it,
-// even using a hardware keyboard in the emulator. I never got to the point of trying it with an
-// on-screen keyboard.
-//
-// Box with high Z-order: This visually ensures our fake dialog's stuff is "on top", but (as with
-// Popup) in ways I don't fully understand, you need to stop touch input sometimes going to the
-// screen underneath and without the separate context (?) created by Popup, the touch input hacks
-// become less reliable. I never actually saw a problem caused by touch input going to the lower
-// screen, but that's not to say it could never happen. (The other miscellaneous Dialog-emulating
-// hacks required by Popup are also required here.)
-//
-// Using an actual full-screen activity which is navigated to and has a full-fledged non-dialog status
-// avoids nearly all of this. Because it *is* a full-fledged screen, there's no "hidden" stuff which
-// could somehow steal touch input or whatever, focus navigation of the contents "just works", the
-// on-screen keyboard "just works" (once you make the appropriate tweaks to AndroidManifest.xml
-// required to make this work anywhere).
-
-// TODO: Move this?
-//
-// Note to self: Rotations are the canonical example of activities being destroyed and re-created
-// fairly casually, but I need to remember they are not the *only* way this happens. In particular,
-// a light/dark theme toggle (which might happen at an arbitrary point because battery saver kicks
-// in, for example) also does this. So even if the app eventually disables rotations for layout
-// reasons, don't assume this gets rid of the need to handle being destroyed and re-created.
-
 // At least early in development, check() and require() would sometimes kill the app but without
 // leaving a clear logcat trace, making it very hard to figure out what went wrong. I am not 100%
 // sure I didn't get confused, I am struggling to reproduce this now. Talking to ChatGPT/Grok
@@ -8443,6 +8394,7 @@ fun ViewPriceHistoryScreen(
 // continuing to use them. I'm just not certain they are necessary. It's possible that having the
 // Log.e() occur *before* an exception is thrown increases the chances the log entry makes it to
 // logcat.
+
 
 inline fun myCheck(condition: Boolean, lazyMessage: () -> String) {
     if (!condition) {
