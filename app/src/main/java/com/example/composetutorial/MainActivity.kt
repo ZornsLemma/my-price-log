57c2168ea4e4d561e404fd1e4a8246baa63349e5
Steven Flintham <sgf@lemma.co.uk>
2025-11-17 20:12:29 +0000

    Add ValidatedNumericTextField
    It isn't used yet, this is a snapshot commit before I try to use it.
    There seem to be two different forms of supportingText used in the code
    I've been over to create this factored out version, I suspect the
    difference is just accidental but we'll have to see.

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4654,6 +4654,54 @@ fun EditSourceScreen(
     }
 }
 
+// TODO: This is WIP, just possibly I don't need some/all of the generic stuff when this is done
+@Composable
+fun <T> ValidatedNumericTextField(
+    value: TextFieldValue,
+    validationRules: List<ValidationRule<String>>,
+    validationRulesKey: Any? = null,
+    allowEmpty: Boolean, // TODO default to = false or force explicit?,
+    validationFlow: SharedFlow<T>,
+    validationFlowFieldId: T,
+    errorHighlightOffset: Dp = defaultErrorHighlightOffset, // TODO JUST MAYBE GET RID OF DEFAULT?
+    baseValidatedTextFieldModifier: Modifier = Modifier,
+    label: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    textStyle: TextStyle = LocalTextStyle.current,
+    onValueChange: (TextFieldValue) -> Unit,
+    enabled: Boolean, // TODO: default to true or force explicit?
+    numericTextFieldModifier: Modifier = Modifier,
+) {
+    BaseValidatedTextField(
+        value.text,
+        validationRules,
+        validationRulesKey,
+        allowEmpty,
+        validationFlow,
+        validationFlowFieldId,
+        errorHighlightOffset,
+        modifier = baseValidatedTextFieldModifier
+    ) { validationResult, interactionSource, validationInputHandle ->
+        NumericTextField(
+            modifier = numericTextFieldModifier.validationInputHandleFocusRequester(validationInputHandle),
+            label = label,
+            value = value,
+            prefix = prefix,
+            suffix = suffix,
+            textStyle = textStyle,
+            onValueChange = onValueChange,
+            enabled = enabled,
+            isError = validationResult != null,
+            supportingText = textOrNull(
+                validationResult,
+                color = MaterialTheme.colorScheme.error
+            ),
+            interactionSource = interactionSource,
+            )
+    }
+}
+
 // TODO: Rename this - it's not about TextFields and is used with various combinations of
 // composables, it is about wrapping up an ErrorHighlightBox with some validation logic.
 @Composable
