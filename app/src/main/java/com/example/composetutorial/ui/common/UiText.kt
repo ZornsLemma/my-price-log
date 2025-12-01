a1151ca7bed01d02c91a5190377546db37adddfb
Steven Flintham <sgf@lemma.co.uk>
2025-12-01 00:55:14 +0000

    Minor tidying
    I did skim over the ChatGPT/Grok suggestions I've deleted, but I think
    after the most recent refactoring what I have is as good as these
    options, to whatever extent they are actually relevant.

--- app/src/main/java/com/example/composetutorial/ui/common/UiText.kt
+++ app/src/main/java/com/example/composetutorial/ui/common/UiText.kt
@@ -7,16 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 sealed class UiText {
-    // TODO: I am not sure, but use of Dynamic in "final" code might be a sign something isn't
-    // right. It might be that it has genuine uses in complex cases where we construct a localised
-    // string via some other means, but it might be a good idea to leave it present but commented
-    // out once I no longer need it.
-    data class Dynamic(val text: String) : UiText()
+    // "Dynamic" works perfectly well, but I suspect using it would indicate something is probably
+    // not correctly translatable. I have commented it out to stop it being used by accident.
+    // data class Dynamic(val text: String) : UiText()
     data class Res(@param:StringRes val resId: Int, val args: List<Any> = emptyList()) : UiText()
     data class PluralsRes(@param:androidx.annotation.PluralsRes val resId: Int, val quantity: Int, val args: List<Any> = emptyList()) : UiText()
 
     fun asString(context: Context): String = when (this) {
-        is Dynamic -> text
+        // is Dynamic -> text
         is Res -> context.getString(resId, *args.toTypedArray())
         is PluralsRes -> context.resources.getQuantityString(resId, quantity, *args.toTypedArray())
     }
