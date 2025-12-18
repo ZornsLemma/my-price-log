1b13eb29f1a6f5d82bc28c07087c510ff5a5c158
Steven Flintham <sgf@lemma.co.uk>
2025-12-18 01:22:41 +0000

    Revert DebugFlags to release settings

--- app/src/main/java/app/zornslemma/mypricelog/debug/DebugConstants.kt
+++ app/src/main/java/app/zornslemma/mypricelog/debug/DebugConstants.kt
@@ -1,9 +1,9 @@
 package app.zornslemma.mypricelog.debug
 
-// TODO: All of these should be set to false for a tagged release unless there's some specific
-// reason to leave them enabled.
+// All of these should be set to false for a tagged release unless there's some specific reason to
+// leave them enabled.
 object DebugFlags {
-    const val USE_STRICT_MODE = true
-    const val LOG_SQL = true
-    const val ALLOW_ROTATION_ON_PHONE = true
+    const val USE_STRICT_MODE = false
+    const val LOG_SQL = false
+    const val ALLOW_ROTATION_ON_PHONE = false
 }
